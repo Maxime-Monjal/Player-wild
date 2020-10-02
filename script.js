3859c0692272980cb2711a5bea260bb90c15313f
fdebrabant <debrabant.fanny@gmail.com>
2020-10-02 15:56:18 +0200

    add style to button Lire la suite

--- script.js
+++ script.js
@@ -2,12 +2,11 @@
 
 const btn = document.querySelector(".btn");
 const menu = document.querySelector(".menuBurger");
-const barre = document.querySelectorAll(".barre")
+
 
 btn.addEventListener("click", function (event) {
   menu.classList.toggle("isOpen");
   btn.classList.toggle("btnCross");
-  
 
 });
 
